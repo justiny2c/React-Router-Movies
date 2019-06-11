21a9ff299a790c63bec350865e3747551938195d
justiny2c <justiny2c@gmail.com>
2019-06-11 16:12:38 -0700

    Done. Added console.log

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -13,6 +13,7 @@ export default class MovieList extends Component {
 
   componentDidMount() {
     axios
+      console.log("Hello")
       .get('http://localhost:5000/api/movies')
       .then(response => {
         this.setState(() => ({ movies: response.data }));
